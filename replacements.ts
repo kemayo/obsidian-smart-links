610908d0d9b83ee03451ccc6af37342c4d23e79e
David Lynch <kemayo@gmail.com>
2022-12-09 11:21:14 -0600

    Make indentation consistent
    Apparently the .editorconfig wasn't respected when I made the new file?
    Not sure what's happening there.

--- replacements.ts
+++ replacements.ts
@@ -8,43 +8,43 @@
 // Desired code: `(?<=^| |\t|\n)` + making the match function simpler.
 
 export class SmartLinksPattern {
-    boundary: RegExp = /(^| |\t|\n)$/;
+	boundary: RegExp = /(^| |\t|\n)$/;
 
 	regexp: RegExp;
-    replacement: string;
+	replacement: string;
 	constructor(pattern: string, replacement: string) {
 		this.regexp = new RegExp(pattern);
-    	this.replacement = replacement;
+		this.replacement = replacement;
 	}
-    match(text: string) : RegExpMatchArray|null {
-        const match = text.match(this.regexp);
-        if (match) {
-            // Because of the above-mentioned lookbehind issue we're doing a
-            // second check here, as a manual lookbehind.
-            const preceding = text.charAt((match.index ?? 0) - 1);
-            if (preceding.match(this.boundary)) {
-                return match;
-            }
-        }
-        return null;
-    }
+	match(text: string) : RegExpMatchArray|null {
+		const match = text.match(this.regexp);
+		if (match) {
+			// Because of the above-mentioned lookbehind issue we're doing a
+			// second check here, as a manual lookbehind.
+			const preceding = text.charAt((match.index ?? 0) - 1);
+			if (preceding.match(this.boundary)) {
+				return match;
+			}
+		}
+		return null;
+	}
 }
 
 export function parseNextLink(text: string, pattern: SmartLinksPattern):
 		| { found: false; remaining: string }
 		| { found: true; preText: string; link: string; href: string; remaining: string }
 {
-    let result, href;
-    result = pattern.match(text);
-    if (result) {
-        href = result[0].replace(pattern.regexp, pattern.replacement);
-    }
-    if (!result || !href) {
-        return { found: false, remaining: text };
-    }
+	let result, href;
+	result = pattern.match(text);
+	if (result) {
+		href = result[0].replace(pattern.regexp, pattern.replacement);
+	}
+	if (!result || !href) {
+		return { found: false, remaining: text };
+	}
 
-    const preText = text.slice(0, result.index);
-    const link = result[0];
-    const remaining = text.slice((result.index ?? 0) + link.length);
-    return { found: true, preText, link, href, remaining };
+	const preText = text.slice(0, result.index);
+	const link = result[0];
+	const remaining = text.slice((result.index ?? 0) + link.length);
+	return { found: true, preText, link, href, remaining };
 }
